cb52ec85940febc59d35da05c40660afcb5657d0
jiebaomaster <jiebaomaster@163.com>
2021-09-23 10:40:30 +0800

    [add]用单调栈维护最小字典序

---
+++ data_structure/单调栈/字典序最小子串/316.去除重复字母.cpp
@@ -0,0 +1,44 @@
+
+/**
+ * https://leetcode-cn.com/problems/remove-duplicate-letters/
+ * https://leetcode-cn.com/problems/remove-duplicate-letters/solution/you-qian-ru-shen-dan-diao-zhan-si-lu-qu-chu-zhong-/
+ * https://leetcode-cn.com/problems/remove-duplicate-letters/solution/yi-zhao-chi-bian-li-kou-si-dao-ti-ma-ma-zai-ye-b-4/
+ * 
+ * 高级字符串去重，求一个字典序最小的不重复子串，
+ * 由单调栈保证字典序最小，附加出栈条件保证每个字符都只出现一次
+ */
+class Solution {
+ public:
+  string removeDuplicateLetters(string s) {
+    stack<char> stack;     // 单调栈保证字典序最小
+    int times[256] = {0};  // 记录在当前位置之后，每个字母还会出现几次
+    bool inStack[256] = {0}; // 记录字母是否出现在栈中
+
+    for (auto c : s) { // 统计每个字母出现的次数
+      times[c]++;
+      inStack[c] = false;
+    }
+
+    for (auto c : s) {
+      times[c]--; // 更新字母还会出现的次数
+
+      if (inStack[c]) continue; // 去重，已经在栈中的元素不重复入栈
+
+      // 对于当前元素 c，为了保持字典序最小，如果 c 小于栈顶元素，有可能需要抛弃栈顶元素
+      // 但是栈顶元素如果在这之后都不会出现了，就不能抛弃栈顶元素，否则就会丢失当前栈顶元素
+      while (!stack.empty() && times[stack.top()] && c < stack.top()) {
+        inStack[stack.top()] = false;
+        stack.pop();
+      }
+      stack.push(c);
+      inStack[c] = true; // 记录字母 c 已经在栈中
+    }
+
+    string res;
+    while (!stack.empty()) {
+      res = string(1, stack.top()) + res; // 栈中的元素要逆序拼接
+      stack.pop();
+    }
+    return res;
+  }
+};
---
+++ data_structure/单调栈/字典序最小子串/402.移掉K位数字.cpp
@@ -0,0 +1,47 @@
+/**
+ * https://leetcode-cn.com/problems/remove-k-digits/
+ * https://leetcode-cn.com/problems/remove-duplicate-letters/solution/yi-zhao-chi-bian-li-kou-si-dao-ti-ma-ma-zai-ye-b-4/
+ * 
+ * 移除某个数中的 k 位数字，使得剩下的数字最小，其实就是在求一个字典序最小的子串
+ * 由单调栈保证字典序最小，附加的出栈条件保证只移除 k 个数字
+ */
+class Solution {
+ public:
+  string removeKdigits(string num, int k) {
+    if (num.size() == 0 || num.size() == k) return "0";
+    // 两个相同位数的数字大小关系取决于第一个不同的数的大小。第一个不同的数小的那个数字更小。
+    // 单调递增栈劲量保留最小的数字，注意保留的顺序是相反的
+    stack<char> s;  
+    int i = 0;
+    while (i < num.size()) {
+      // 每次出栈代表有一个数字被移除，最多移除 k 个数字
+      while (!s.empty() && k > 0 && num[i] < s.top()) {
+        s.pop();
+        k--;
+      }
+      s.push(num[i]);
+      i++;
+    }
+    string res;
+    while (!s.empty()) {
+      // k > 0 代表移除的数还不够多，此时要移除栈顶的数凑数，因为栈顶的数更大
+      if (k > 0) {
+        k--;
+        s.pop();
+        continue;
+      }
+      // 将栈中的数逆序拼接成字符串
+      res = string(1, s.top()) + res;
+      s.pop();
+    }
+
+    // 数字要去除前导 0，空串要返回0
+    auto pos = res.find_first_not_of('0');
+    if (pos == string::npos)
+      return "0";
+    else {
+      string ss = res.substr(pos);
+      return ss.size() == 0 ? "0" : ss;
+    }
+  }
+};
