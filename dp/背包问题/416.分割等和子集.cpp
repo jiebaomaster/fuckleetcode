c486b5e5c8255a2e09374dd8465f5a9ce78ab2a5
jiebaomaster <jiebaomaster@163.com>
2021-09-30 16:08:44 +0800

    [add]用动态规划解决背包问题

---
+++ dp/背包问题/416.分割等和子集.cpp
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode-cn.com/problems/partition-equal-subset-sum/
+ * https://labuladong.gitbook.io/algo/mu-lu-ye-2/mu-lu-ye-2/bei-bao-zi-ji
+ * 子集划分问题，我们可以先对集合求和，得出 sum，从而把问题转化为下列背包问题：
+ * 给一个可装载重量为 sum / 2 的背包和 N 个物品，每个物品的重量为 nums[i]。现在让你装物品，是否存在一种装法，能够恰好将背包装满
+ * dp[i][j] 表示对于前 i 个物品，是否存在一种装法，恰好装入容量为 j 的背包中
+ * 对于每个物品 nums[i]，只有装入和不装入两种状态
+ * 1. 如果不把第 i 个物品装入背包，那么是否能够恰好装满背包，
+ *      取决于前面 i-1 个物品能否装满容量为 j 的背包，即 dp[i-1][j]
+ * 2. 如果把第 i 个物品装入背包，那么是否能够恰好装满背包，
+ *      取决于前面 i-1 个物品能否装满容量为 j-nums[i] 的背包，即 dp[i-1][j-nums[i]]。
+ * 3. 当背包剩余空间不能装下第 i 个物品时，只能选择不装入物品 i 的情况
+ */
+class Solution {
+ public:
+  bool canPartition(vector<int>& nums) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    if (sum % 2) return false; // 奇数肯定不能划分成等和的两份
+    sum /= 2;
+    vector<vector<bool>> dp(nums.size() + 1, vector<bool>(sum + 1));
+    // 要装满一个空间为 0 的背包，只要什么都不装就可以了
+    for (int i = 0; i <= nums.size(); i++) dp[i][0] = true;
+    // 当没有物品可选择的时候，肯定没办法装满背包
+    for (int j = 1; j <= sum; j++) dp[0][j] = false;
+
+    for (int i = 1; i <= nums.size(); i++) {
+      for (int j = 1; j <= sum; j++) {
+        if (j < nums[i - 1]) // 背包的剩余空间太小，当前物品不能装入
+          dp[i][j] = dp[i - 1][j];
+        else // 当前物品可以装入也可以不装入，只要有一个满足即可
+          dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+      }
+    }
+    return dp[nums.size()][sum];
+  }
+};
---
+++ dp/背包问题/518.零钱兑换2-所有可行方法.cpp
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode-cn.com/problems/coin-change-2/
+ * https://labuladong.gitbook.io/algo/mu-lu-ye-2/mu-lu-ye-2/bei-bao-ling-qian
+ * 将本题转换为背包问题的表述：有一个背包，最大容量为 amount，有一系列物品 coins，
+ * 每个物品的重量为 coins[i]，每个物品的数量无限。请问有多少种方法，能够把背包恰好装满？
+ * 
+ * dp[i][j] 表示只使用前 i 个物品，恰好装入容量为 j 的背包的方法数
+ * 对于每个物品 coins[i]，只有装入和不装入两种状态
+ * 1. 如果不把第 i 个物品装入背包，那么恰好装满背包的方法数，
+ *      取决于使用前面 i-1 个物品装满容量为 j 的背包的方法数，即 dp[i-1][j]
+ * 2. 如果把第 i 个物品装入背包，那么恰好装满背包的方法数，
+ *      取决于使用前面 i 个物品装满容量为 j-coins[i] 的背包的方法数，即 dp[i][j-coins[i]]。
+ *     （本题为完全背包问题，每个物品数量无限，当使用了第i个物品后，还能再次使用）
+ * 3. 当背包剩余空间不能装下第 i 个物品时，只能选择不装入物品 i 的情况
+ */
+class Solution {
+ public:
+  int change(int amount, vector<int>& coins) {
+    vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1));
+    for (int i = 0; i <= coins.size(); i++) dp[i][0] = 1;
+    for (int j = 1; j <= amount; j++) dp[0][j] = 0;
+
+    for (int i = 1; i <= coins.size(); i++) {
+      for (int j = 1; j <= amount; j++) {
+        if (j < coins[i - 1]) // 背包的剩余空间太小，当前物品不能装入
+          dp[i][j] = dp[i - 1][j];
+        else // 总的方法数为装入和不装入两种方法数之和
+          dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+      }
+    }
+    return dp[coins.size()][amount];
+  }
+};
